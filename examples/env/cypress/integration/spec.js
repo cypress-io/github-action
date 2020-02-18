2c478fd04169bc9a99baf5ed3a857435dcad42bd
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-02-18 16:47:19 -0500

    pass with prefix

--- examples/env/cypress/integration/spec.js
+++ examples/env/cypress/integration/spec.js
@@ -1,6 +1,9 @@
 it('has all expected env variables', () => {
   // environmentName is set as workflow environment variable
-  expect(Cypress.env('environmentName')).to.equal('staging')
+  expect(
+    Cypress.env('environmentName'),
+    'has environment name'
+  ).to.equal('staging')
 
   // host and port are set via action's "with: env:" parameter
   expect(Cypress.env()).to.deep.include({
