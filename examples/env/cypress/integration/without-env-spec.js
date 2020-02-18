96bf83ca2440e55c71fb7b47f3efeb553a7d6017
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-02-18 16:45:08 -0500

    separate specs

--- examples/env/cypress/integration/without-env-spec.js
+++ examples/env/cypress/integration/without-env-spec.js
@@ -1,10 +1,4 @@
 it('has all expected env variables', () => {
   // environmentName is set as workflow environment variable
   expect(Cypress.env('environmentName')).to.equal('staging')
-
-  // host and port are set via action's "with: env:" parameter
-  expect(Cypress.env()).to.deep.include({
-    host: 'api.dev.local',
-    port: 4222
-  })
 })
