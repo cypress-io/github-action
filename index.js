509d9749fd7f1c49ab8c3a5684d05d4fec2024c9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-11-13 15:09:28 -0500

    fix: prevent Cypress binary snowball

--- index.js
+++ index.js
@@ -111,6 +111,8 @@ const getNpmCache = () => {
     o.inputPath = NPM_CACHE_FOLDER
   }
 
+  // use exact restore key to prevent NPM cache from growing
+  // https://glebbahmutov.com/blog/do-not-let-npm-cache-snowball/
   o.restoreKeys = o.primaryKey = key
   return o
 }
@@ -131,7 +133,12 @@ const getCypressBinaryCache = () => {
     inputPath: CYPRESS_CACHE_FOLDER,
     restoreKeys: `cypress-${platformAndArch}-`
   }
-  o.primaryKey = o.restoreKeys + lockHash()
+  const hash = lockHash()
+  const key = `cypress-${platformAndArch}-${hash}`
+
+  // use only exact restore key to prevent cached folder growing in size
+  // https://glebbahmutov.com/blog/do-not-let-cypress-cache-snowball/
+  o.restoreKeys = o.primaryKey = key
   return o
 }
 
