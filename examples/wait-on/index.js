47368812c9039784535fc50686af2c9c1b5976d4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-11-13 13:12:30 -0500

    add server that starts after 7 seconds

---
+++ examples/wait-on/index.js
@@ -0,0 +1,18 @@
+// always log messages
+// useful because shows timestamps
+const log = require('debug')('*')
+const http = require('http')
+
+log('starting the server...')
+setTimeout(function() {
+  const port = 3050
+  log('creating the server on port %d', port)
+  const server = http.createServer((req, res) => {
+    log('request %s %s', req.method, req.url)
+    res.writeHead(200)
+    res.end('all good')
+  })
+  server.listen(port, () => {
+    log('server is listening')
+  })
+}, 7000)
