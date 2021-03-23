fcb5d15b5fd986c0331277ccf7228615f189cb95
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-03-23 15:39:39 -0400

    make recording test on purpose

--- examples/basic/cypress/integration/spec.js
+++ examples/basic/cypress/integration/spec.js
@@ -1,6 +1,6 @@
 it('works', () => {
   expect(42).to.equal(21 + 21)
   cy.visit('https://example.cypress.io').then(() => {
-    expect('hello').to.equal('bye')
+    expect('hello').to.equal('hello')
   })
 })
--- examples/recording/cypress/integration/spec-a.js
+++ examples/recording/cypress/integration/spec-a.js
@@ -1,4 +1,5 @@
 it('spec-A works', () => {
-  expect(42).to.equal(21 + 21)
+  // make fail on purpose
+  expect(42).to.equal(21 + 22)
   cy.visit('https://example.cypress.io').wait(2000)
 })
