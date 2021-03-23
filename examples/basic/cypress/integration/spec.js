b900bb785402701942376bc3a5e649074b532f88
Gleb Bahmutov <bahmutov@yahoo.com>
2021-03-23 15:56:51 -0400

    fix: slightly better error message if tests cannot be found (#329)

--- examples/basic/cypress/integration/spec.js
+++ examples/basic/cypress/integration/spec.js
@@ -1,4 +1,6 @@
 it('works', () => {
   expect(42).to.equal(21 + 21)
-  cy.visit('https://example.cypress.io')
+  cy.visit('https://example.cypress.io').then(() => {
+    expect('hello').to.equal('hello')
+  })
 })
