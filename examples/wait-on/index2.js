bf8a3ebf12ef6a8210c3dc8cc32fda997b11d17a
Gleb Bahmutov <bahmutov@yahoo.com>
2021-03-30 10:38:09 -0400

    Add more waiting on the server to start examples (#334)
    * add server with 50 second delay
    
    * add example with waiting 130 seconds
    
    * add test for the server that starts after 100 seconds

--- examples/wait-on/index2.js
+++ examples/wait-on/index2.js
@@ -8,15 +8,20 @@ const http = require('http')
 const arg = require('arg')
 
 const args = arg({
-  '--port': Number
+  '--port': Number,
+  '--delay': Number
 })
 const port = args['--port'] || 3050
+const errorPeriodSeconds = args['--delay'] || 10
 
-const errorPeriodSeconds = 10
 const endErrorsAt = +new Date() + errorPeriodSeconds * 1000
 
 log('creating the server on port %d', port)
 log('will respond with errors for %d seconds', errorPeriodSeconds)
+
+setTimeout(() => {
+  log('server will start responding with OK from now on')
+}, errorPeriodSeconds * 1000)
 
 const server = http.createServer((req, res) => {
   log('request %s %s', req.method, req.url)
