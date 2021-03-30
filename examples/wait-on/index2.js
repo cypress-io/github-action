e09a789c07de5df71ad88643e5fae8b414e7ae6d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-03-30 10:21:29 -0400

    add test for the server that starts after 100 seconds

--- examples/wait-on/index2.js
+++ examples/wait-on/index2.js
@@ -19,6 +19,10 @@ const endErrorsAt = +new Date() + errorPeriodSeconds * 1000
 log('creating the server on port %d', port)
 log('will respond with errors for %d seconds', errorPeriodSeconds)
 
+setTimeout(() => {
+  log('server will start responding with OK from now on')
+}, errorPeriodSeconds * 1000)
+
 const server = http.createServer((req, res) => {
   log('request %s %s', req.method, req.url)
   if (new Date() < endErrorsAt) {
