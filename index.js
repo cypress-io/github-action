1bcfde8d6a069b488c477e08fc9f3deebc3ecb72
Gleb Bahmutov <bahmutov@yahoo.com>
2021-05-03 22:39:58 -0400

    fix: send ci-build-id only in parallel or group mode (#366)

--- index.js
+++ index.js
@@ -540,8 +540,7 @@ const runTestsUsingCommandLine = async () => {
     cmd.push(quoteArgument(configFileInput))
   }
 
-  if (record) {
-    // only include the ci-build-id if recording
+  if (parallel || group) {
     const { branch, buildId } = await getCiBuildId()
     if (branch) {
       core.exportVariable('GH_BRANCH', branch)
@@ -662,7 +661,7 @@ const runTests = async () => {
     cypressOptions.env = core.getInput('env')
   }
 
-  if (cypressOptions.record) {
+  if (cypressOptions.parallel || cypressOptions.group) {
     const { branch, buildId } = await getCiBuildId()
     if (branch) {
       core.exportVariable('GH_BRANCH', branch)
