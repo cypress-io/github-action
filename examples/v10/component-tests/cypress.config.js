41644e4c7241e3520e2dbaada6c58c32f88f47a5
Robert Guss <robertguss@pm.me>
2022-02-15 13:27:35 -0500

    added supportFile: false to cypress.config.js for component-tests

--- examples/v10/component-tests/cypress.config.js
+++ examples/v10/component-tests/cypress.config.js
@@ -1,9 +1,10 @@
-const { defineConfig } = require("cypress");
-const { devServer } = require("@cypress/react/plugins/react-scripts");
+const { defineConfig } = require('cypress')
+const { devServer } = require('@cypress/react/plugins/react-scripts')
 
 module.exports = defineConfig({
   component: {
     devServer,
-    specPattern: "src/**/*.cy.{js,jsx,ts,tsx}",
-  },
-});
+    specPattern: 'src/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: false
+  }
+})
