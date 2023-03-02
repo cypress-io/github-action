4160b9ded26d298754f037046412bb462a9b26da
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2023-03-02 16:08:15 +0100

    Add scripts to update examples to cypress@latest (#779)
    * build: scripts to update examples to cypress@latest
    
    Add MAINTENANCE document to new docs/ directory
    Extend markdown link check to docs/
    
    * Correct markdown typo
    
    * Update docs/MAINTENANCE.md
    
    Co-authored-by: Bill Glesias <bglesias@gmail.com>
    
    ---------
    
    Co-authored-by: Matt Henkes <mjhenkes@gmail.com>
    Co-authored-by: Bill Glesias <bglesias@gmail.com>

---
+++ scripts/update-cypress-latest-npm.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# All examples using npm technology are updated to
+# Cypress latest version
+#
+npmExamples=(
+    'basic'
+    'chrome'
+    'component-tests'
+    'config'
+    'custom-command'
+    'env'
+    'firefox'
+    'install-only'
+    'node-versions'
+    'quiet'
+    'react-scripts'
+    'recording'
+    'start'
+    'wait-on'
+    'wait-on-vite'
+    'webpack'
+    )
+
+echo updating npm examples to Cypress latest version
+cd examples
+for i in ${!npmExamples[@]}; do
+echo
+echo updating examples/${npmExamples[$i]} to cypress@latest
+cd ${npmExamples[$i]}
+npm install cypress@latest --save-dev --save-exact
+npm ls cypress
+cd ..
+done
+cd ..
