db1693016f23ccf9043f4b2428f9b04e5d502a73
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2023-06-02 00:52:00 +0200

    test: migrate plugins to config (#926)

--- examples/env/cypress.config.js
+++ examples/env/cypress.config.js
@@ -4,7 +4,12 @@ module.exports = defineConfig({
   fixturesFolder: false,
   e2e: {
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      console.log('logging from cypress.config.js')
+      console.log(
+        'process.env.CYPRESS_environmentName',
+        process.env.CYPRESS_environmentName
+      )
+      console.log('entire config.env', config.env)
     },
     supportFile: false,
   },
