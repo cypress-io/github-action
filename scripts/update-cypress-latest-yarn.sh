488f8291fabf72f3b67dd6ae7eb7ac0bd5ed5e57
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2024-04-01 15:44:46 +0200

    build: separate yarn and pnpm maintenance scripts (#1163)

--- scripts/update-cypress-latest-yarn.sh
+++ scripts/update-cypress-latest-yarn.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e # fail on error
 #
-# Examples using yarn and pnpm package managers are
+# Examples using the yarn package manager are
 # updated to Cypress latest version
 #
-# Make sure that pnpm and yarn are installed
-./scripts/check-package-managers-other.sh
+# Make sure that yarn is installed
+./scripts/check-package-manager-yarn.sh
 
 echo updating yarn examples to Cypress latest version
 cd examples
@@ -51,26 +51,6 @@ echo updating examples/yarn-modern-pnp to cypress@latest
 cd yarn-modern-pnp
 yarn set version latest
 yarn add cypress --dev --exact
-cd ..
-
-# examples/basic-pnpm (pnpm)
-echo
-echo updating pnpm example to Cypress latest version
-echo
-echo updating examples/basic-pnpm to cypress@latest
-cd basic-pnpm
-pnpm add cypress@latest --save-dev --save-exact
-pnpm ls cypress
-cd ..
-
-# examples/start-and-pnpm-workspaces
-echo
-echo updating pnpm workspaces example to Cypress latest version
-echo
-echo updating examples/start-and-pnpm-workspaces to cypress@latest
-cd start-and-pnpm-workspaces
-pnpm update cypress --latest --recursive
-pnpm ls cypress --recursive
 cd ..
 
 cd ..
