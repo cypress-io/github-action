013dd060a557fd6d28b933ed1c3c53f9f3f1c069
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2025-02-26 14:19:49 +0100

    chore: extend linting to cypress examples (#1373)
    * chore: add examples linting
    
    * remove unused setupnodeevents or parameters
    
    * selective disable cypress/no-unnecessary-waiting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,16 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
+import pluginCypress from 'eslint-plugin-cypress/flat'
 
 export default [
   pluginJs.configs.recommended,
-  { name: 'global-ignores', ignores: ['dist/', 'examples/'] },
+  pluginCypress.configs.recommended,
   {
-    name: `all-js`,
+    name: 'global-ignores',
+    ignores: ['dist/', 'examples/nextjs/src/app/']
+  },
+  {
+    name: 'all-js',
     languageOptions: {
       globals: {
         ...globals.browser,
--- examples/webpack/cypress.config.js
+++ examples/webpack/cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   fixturesFolder: false,
   e2e: {
-    setupNodeEvents(on, config) {},
     supportFile: false,
     baseUrl: 'http://localhost:8080',
   },
