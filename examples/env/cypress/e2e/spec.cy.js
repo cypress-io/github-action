9d40458b45f7e23917d4cb731a52b6b2c3823b8d
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2026-09-08 17:31:54 +0200

    Use on.cypress.io for migration documentation
    Co-authored-by: Jennifer Shehane <shehane.jennifer@gmail.com>

--- examples/env/cypress/e2e/spec.cy.js
+++ examples/env/cypress/e2e/spec.cy.js
@@ -3,7 +3,7 @@ import semver from 'semver'
 // Cypress.env is deprecated in Cypress 15.10.0 and removed in
 // Cypress 16.0.0. The test is retained as an example for Cypress <15.10.0 users
 // See the migration guide for more information:
-// https://docs.cypress.io/guides/references/migration-guide#Cypress-env
+// https://on.cypress.io/migration-guide#Migrating-away-from-Cypressenv
 
 if (semver.gte(Cypress.version, '15.10.0')) {
   it('skipping Cypress.env tests for Cypress >=15.10.0', () => {})
